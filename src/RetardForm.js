5e0a8864dacaa7755202870eb0d6980bd71bddba
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2018-06-07 15:57:54 -0400

    server can handle retard only et causes

--- src/RetardForm.js
+++ src/RetardForm.js
@@ -1,3 +1,4 @@
+
 import React,{Component} from 'react'
 import TrainId_data from './TrainId_data'
 import { Dropdown } from 'semantic-ui-react'
@@ -5,6 +6,7 @@ import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'semantic-ui-css/semantic.min.css';
+import isHoliday from 'holidays-nordic'
 import LoadingSpinner from './LoadingSpinner'
 const pointe_data = {"AM":"AM","PM":"PM"}
 
@@ -20,11 +22,15 @@ export default class RetardForm extends Component {
       selected_dpvalues: null,
       pointe_selection: "AM",
       startDate: moment(),
-      prediction:null
+      prediction:null,
+      causes:-1,
+      commentaires:''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handlePointeChange = this.handlePointeChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCauseChange = this.handleCauseChange.bind(this);
+    this.handleComChange = this.handleComChange.bind(this);
   }
   //quand l UI est monte alors...
   componentWillMount() {
@@ -43,6 +49,20 @@ export default class RetardForm extends Component {
   handlePointeChange(event) {
     this.setState({pointe_selection: event.target.value});
   }
+  handleComChange(event) {
+    event.preventDefault();
+    this.setState({
+    
+      commentaires: event.target.value
+    });
+
+  }
+  handleCauseChange(event) {
+    this.setState({
+    
+      causes: event.target.value
+    });
+  }
   handleChange(date) {
     this.setState({
     
@@ -52,7 +72,8 @@ export default class RetardForm extends Component {
   handleSubmit(event) {
     event.preventDefault();
      console.log(this.state.pointe_selection)
-     console.log(this.state.startDate._d.toString())
+     console.log("Holiday:",isHoliday(this.state.startDate,'no'))
+     console.log(moment(this.state.startDate).format('MM/DD/YYYY'))
      let selected_dpvalues ={}
      for (let k of this.state.selected_dpvalues ) {
       selected_dpvalues[k]=k
@@ -61,13 +82,16 @@ export default class RetardForm extends Component {
      console.log(selected_dpvalues)
      
      //ifconfig -a wlan
-     const query_url = "http://192.168.0.104:5000/predict"
+     const query_url = "http://10.62.73.233:5000/predict"
      
     const mybody = {
       'type_prediction':"retard-only",
-      'date':this.state.startDate._d.toString(),
+      'date':moment(this.state.startDate).format('MM/DD/YYYY'),
       'train':selected_dpvalues,
-      'pointe':this.state.pointe_selection
+      'pointe':this.state.pointe_selection,
+      'ferie': isHoliday(this.state.startDate,'no') ? 1 : 0,
+      'causes':this.state.causes,
+      'commentaires':this.state.commentaires
     }
     this.setState({ loading: true })
      fetch(query_url, 
@@ -102,7 +126,7 @@ handleDropdownChange =(e, {value}) => {
         <form onSubmit={this.handleSubmit} className="form-inline">
              <div className="container">
              <div className="row">
-              <div className="col-md-3">
+              <div className="col-md-2">
               <div className="form-group">
                  <label>Id Train</label>
                  <Dropdown fluid selection 
@@ -116,14 +140,14 @@ handleDropdownChange =(e, {value}) => {
                           disabled={false}  />
               </div>
               </div>
-              <div className="col-md-3">
+              <div className="col-md-2">
                  <div className="form-group">
                    <label>Date (futur ou passé)</label>
                   <DatePicker selected={this.state.startDate}  
                            onChange={this.handleChange} />
                  </div>
               </div>
-              <div className="col-md-3">
+              <div className="col-md-2">
                  <div className="form-group">
                    <label>Pointe:</label>
                       <select value={this.state.pointe_selection} 
@@ -134,13 +158,37 @@ handleDropdownChange =(e, {value}) => {
                       </select>
                 </div>
               </div>
+
+              <div className="col-md-2">
+                 <div className="form-group">
+                   <label>Causes:</label>
+                      <select value={this.state.causes} 
+                              onChange={this.handleCauseChange} 
+                              className="form-control">
+                        <option value ={-1}></option>
+                        <option value ={4}>Materiel roulant</option>
+                        <option value={0}>Autres</option>
+                        <option value ={2}>Exploitation</option>
+                        <option value={3}>Infrastructure</option>
+                        <option value={1}>Clients</option>
+                      </select>
+                </div>
+              </div>
               <div className="col-md-3">
+                 <div className="form-group">
+                   <label>Commentaires:</label>
+                      <textarea onChange={this.handleComChange} value= {this.state.commentaires}></textarea>
+                </div>
+              </div>
+
+             
+              </div></div>
+              {this.state.loading ? <LoadingSpinner /> :  <p>{this.state.prediction}</p> }
+              <div className="col-md-2">
                  <div className="form-group">
                    <button  className="btn btn-primary btn-lg">Predict</button>
                 </div>
-              </div></div></div>
-              {this.state.loading ? <LoadingSpinner /> :  <p>{this.state.prediction}</p> }
-             
+              </div>
            </form>
            
            )
